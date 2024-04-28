0a7a8dd56c48f6ee89dd3ff677e52a35647279ab
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2024-04-28 20:42:58 -0300

    feat: criar_carona_motorista

--- src/Logic/CaronaLogic.pl
+++ src/Logic/CaronaLogic.pl
@@ -1,16 +1,22 @@
 :- module(_,[
     possui_carona_origem_destino/2, 
     mostra_caronas_origem_destino/3, 
-    mostrar_caronas_passageiro_participa/2
+    mostrar_caronas_passageiro_participa/2,
+    criar_carona_motorista/6
     ]).
 
-:- use_module('src/Schemas/CsvModule.pl').
-:- use_module('src/Model/Carona.pl').
+:- use_module('../Schemas/CsvModule.pl').
+:- use_module('../Model/Carona.pl').
 
 % Définir le chemin du fichier CSV comme une variable globale
-csv_file('database/caronas.csv').
+csv_file('../../database/caronas.csv').
 destinos_column(4).
 passageiros_column(6).
+
+% Fato dinâmico para gerar o id das caronas
+id(0).
+incrementa_id :- retract(id(X)), Y is X + 1, assert(id(Y)).
+:- dynamic id/1.
 
 % Predicate to check if a route exists from Origem to Destino
 possui_carona_origem_destino(Origem, Destino):-
@@ -51,3 +57,14 @@ mostrar_caronas_passageiro_participa(PassageiroCpf, CaronasStr):-
     passageiros_column(Pass_Column),
     read_csv_row_by_list_element(File, Pass_Column, PassageiroCpf, Rows),
     findall(Str, (member(Row, Rows), caronaToStr(Row, Str)), CaronasStr).
+
+criar_carona_motorista(Hora, Data, Destinos, MotoristaCpf, Valor, NumPassageirosMaximos) :-
+    csv_file(CsvFile),
+    id(ID),
+    Carona = carona(ID, Hora, Data, Destinos, MotoristaCpf, [], Valor, naoIniciada, NumPassageirosMaximos, -1),
+    incrementa_id,
+    % Converte a instância de Carona em uma lista
+    carona_to_list(Carona, ListaCarona),
+    % Escreve a linha no arquivo CSV
+    write_csv_row_all_steps(CsvFile, ListaCarona).
+    
