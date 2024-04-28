76fe64843346df875c66f478c763d9d6dc453862
Filipe José <filipe1417@gmail.com>
2024-04-28 19:10:46 -0300

    adicionadas funcoes de validação

--- src/Util/Util.pl
+++ src/Util/Util.pl
@@ -1,5 +1,26 @@
-:- module(_, [validar_cpf/1]).
+:- module(_, [validar_cpf/1, null_or_empty/1, validar_email/1, validar_genero/1]).
 
 validar_cpf(CPF):-
     atom_length(CPF, Length),
     Length = 11.
+
+% n sei se é gambiarra
+null_or_empty(STRING):-
+    atom_length(STRING, Length),
+    Length = 0.
+
+validar_email(Email) :-
+    atom_chars(Email, Chars),
+    split_string(Email, "@", "", [_Usuario, Dominio]),
+    length(Chars, Length),
+    Length > 0, % Verifica se a string não está vazia
+    atom_length(Dominio, LengthDominio),
+    LengthDominio > 0, % Verifica se o domínio não está vazio
+    split_string(Dominio, ".", "", [_NomeDominio, Extensao]),
+    Extensao \= "". % Verifica se a extensão do domínio não está vazia
+
+validar_genero(Genero):- 
+    Genero = "M"; Genero = "F"; Genero = "NB".
+
+    
+
