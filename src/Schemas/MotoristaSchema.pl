53077967d26cf4225b3329753cd1a35b7b852086
ianzx15 <ian.rodrigues@ccc.ufcg.edu.br>
2024-04-28 16:55:22 -0300

    feat: update schema

--- src/Schemas/MotoristaSchema.pl
+++ src/Schemas/MotoristaSchema.pl
@@ -1,45 +1,53 @@
 :- dynamic motorista/9.
 
-:-use_module('CsvModule.pl').
-
-:- use_module("../Model/Motorista.pl").
+:- use_module("CsvModule.pl").
+:- use_module("../Model/MotoristaModel.pl").
 
 % motorista(CPF, CEP, Nome, Email, Telefone, Senha, CNH, Genero, Regiao)
 
 
+file(Retorno):-
+    Retorno = '../../database/motoristas.csv'.
+
 % Predicados para cadastro de motorista
 cadastra_motorista(CPF, Nome, Email, Telefone, Senha, CNH, CEP, Genero, Regiao, Retorno) :-
     % Verificar se o CPF já está cadastrado
-    load_motorista_facts('../Schemas/motoristas.csv'),
     ( motorista(CPF, _, _, _, _, _, _, _, _) ->
-        write('CPF já cadastrado')
-    ; % Se não estiver cadastrado, realizar o cadastro
-        motoristaToStr(motorista(cpf(CPF), nome(Nome), email(Email), senha(Senha), telefone(Telefone), cnh(CNH), cep(CEP), genero(Genero), regiao(Regiao)), Str),
-        write_csv_row('motoristas.csv', [row(CPF, Nome, Email, Telefone, Senha, CNH, CEP, Genero, Regiao)]),
-        assert_unique_motorista_fact(row(CPF, Nome, Email, Telefone, Senha, CNH, CEP, Genero, Regiao)),
-        %assertz(motorista(cpf(CPF), nome(Nome), email(Email), senha(Senha), telefone(Telefone), cnh(CNH), cep(CEP), genero(Genero), regiao(Regiao))),
-        Retorno = 'Cadastro realizado com sucesso'
+        Retorno ='CPF já cadastrado'
+    ; motorista(_, _, _, _, _, _, CNH, _, _) ->
+        Retorno = 'CNH já cadastrada'
+        ;
+            %Se não estiver cadastrado, realizar o cadastro
+            motoristaToStr(motorista(cpf(CPF), nome(Nome), email(Email), senha(Senha), telefone(Telefone), cnh(CNH), cep(CEP), genero(Genero), regiao(Regiao)), Str),
+            assert(motorista(CPF,Nome,Email,Senha,Telefone,CNH,CEP,Genero,Regiao)),
+            write_csv_row('../../database/motoristas.csv', [row(CPF, Nome, Email, Telefone, Senha, CNH, CEP, Genero, Regiao)]),
+            Retorno = 'Cadastro realizado com sucesso'
     ).
 
 
 % Predicado para remoção de motorista
-remove_motorista(CPF) :-
-    retract(motorista(CPF, _, _, _, _, _, _, _, _)).
+remove_motorista(CPF, Retorno) :-
+    file(Caminho),
+    delete_csv_row(Caminho, 1, CPF),
+    Retorno = 'Motorista removido com sucesso.'.
+  
 
 % Predicado para atualização de motorista
-atualiza_motorista(CPF, Campo, NovoValor) :-
-    retract(motorista(CPF, CEP, Nome, Email, Telefone, Senha, CNH, Genero, Regiao)),
+atualiza_motorista(Chave, Campo, NovoValor, Retorno) :-
+    file(Caminho),
+    % Recupera os detalhes do motorista por CPF
+    read_csv_row(Caminho, 1, Chave, [row(CPF, Nome, Email, Telefone, Senha, CNH, CEP, Genero, Regiao)]),
+    write('sim'),
+    delete_csv_row(Caminho,1,chave),
     % Atualiza apenas o campo desejado
-    (   Campo == 'cep' -> NovoCEP = NovoValor, assertz(motorista(CPF, NovoCEP, Nome, Email, Telefone, Senha, CNH, Genero, Regiao))
-    ;   Campo == 'nome' -> NovoNome = NovoValor, assertz(motorista(CPF, CEP, NovoNome, Email, Telefone, Senha, CNH, Genero, Regiao))
-    ;   Campo == 'email' -> NovoEmail = NovoValor, assertz(motorista(CPF, CEP, Nome, NovoEmail, Telefone, Senha, CNH, Genero, Regiao))
-    ;   Campo == 'telefone' -> NovoTelefone = NovoValor, assertz(motorista(CPF, CEP, Nome, Email, NovoTelefone, Senha, CNH, Genero, Regiao))
-    ;   Campo == 'senha' -> NovaSenha = NovoValor, assertz(motorista(CPF, CEP, Nome, Email, Telefone, NovaSenha, CNH, Genero, Regiao))
-    ;   Campo == 'cnh' -> NovaCNH = NovoValor, assertz(motorista(CPF, CEP, Nome, Email, Telefone, Senha, NovaCNH, Genero, Regiao))
-    ;   Campo == 'genero' -> NovoGenero = NovoValor, assertz(motorista(CPF, CEP, Nome, Email, Telefone, Senha, CNH, NovoGenero, Regiao))
-    ;   Campo == 'regiao' -> NovaRegiao = NovoValor, assertz(motorista(CPF, CEP, Nome, Email, Telefone, Senha, CNH, Genero, NovaRegiao))
-    ;   true % Se o campo não for nenhum dos especificados, mantém o motorista sem alteração
-    ).
+    (   Campo == 'telefone' -> write_csv_row(Caminho,[CPF, Nome, Email, NovoValor, Senha, CNH, CEP, Genero, Regiao])
+    ;   Campo == 'senha' -> write_csv_row(Caminho, [CPF, Nome, Email, Telefone, NovoValor, CNH, CEP, Genero, Regiao])
+    ;   Campo == 'cnh' -> write_csv_row(Caminho, [CPF, Nome, Email, Telefone, Senha, NovoValor, CEP, Genero, Regiao])
+    ;   Campo == 'cep' -> write_csv_row(Caminho, [CPF, Nome, Email, Telefone, Senha, CNH, NovoValor, Genero, Regiao])
+    ;   Campo == 'genero' -> write_csv_row(Caminho, [CPF, Nome, Email, Telefone, Senha, CNH, CEP, NovoValor, Regiao])
+    ;   Campo == 'regiao' -> write_csv_row(Caminho, [CPF, Nome, Email, Telefone, Senha, CNH, CEP, Genero, NovoValor])
+    ;   true
+    ).  
 
 % Predicado para recuperar todos os motoristas de uma determinada região
 recupera_motoristas_por_regiao(Regiao, Motoristas) :-
@@ -47,9 +55,13 @@ recupera_motoristas_por_regiao(Regiao, Motoristas) :-
             motorista(CPF, CEP, Nome, Email, Telefone, Senha, CNH, Genero, Regiao),
             Motoristas).
 
+recupera_motoristas_por_cpf(CPF, Motorista) :-
+    motorista(CPF, Nome, Email, Senha, Telefone, CNH, CEP, Genero, Regiao),
+    Motorista = motorista(CPF, Nome, Email, Telefone, Senha, CNH, CEP, Genero, Regiao).
+
 % Predicado para verificar se o banco de dados está vazio
 banco_vazio :- 
-    load_motorista_facts('../Schemas/motoristas.csv'),
+    load_motorista_facts('../database/motoristas.csv'),
     \+ motorista(_, _, _, _, _, _, _, _, _).
 
 
@@ -62,3 +74,4 @@ escrever_motoristas_csv(Arquivo) :-
     ;   true % Encerra a busca quando não houver mais motoristas
     ),
     close(Stream).
+
