8cfa349ff8156402624da3aa4bd01baa46733c9d
CarmelitaBraga <carmelitabraga8@gmail.com>
2024-04-29 10:48:20 -0300

    feat: select_row_read

--- src/Schemas/CsvModule.pl
+++ src/Schemas/CsvModule.pl
@@ -3,13 +3,6 @@
 :- use_module(library(csv)).
 :- use_module('../Util/Utils.pl').
 
-% select_row([], _, _, []).
-% select_row([Row|Rest], Column, Value, [Row|NewRest]) :-
-%     arg(Column, Row, Value), !,
-%     select_row(Rest, Column, Value, NewRest).
-% select_row([_|Rest], Column, Value, NewRest) :-
-%     select_row(Rest, Column, Value, NewRest).
-
 select_row([], _, _, []).
 select_row([Row|Rest], Column, Value, [Row|NewRest]) :-
     arg(Column, Row, ArgValue),
@@ -44,7 +37,15 @@ write_csv_row_all_steps(File, Data) :-
 % Read a specific row
 read_csv_row(File, Column, Value, Row) :-
     csv_read_file(File, Data),
-    select_row(Data, Column, Value, Row).
+    select_row_read(Data, Column, Value, Row).
+
+select_row_read([], _, _, []).
+select_row_read([Row|Rest], Column, Value, [Row|NewRest]) :-
+    arg(Column, Row, ArgValue),
+    ArgValue == Value, !,
+    select_row_read(Rest, Column, Value, NewRest).
+select_row_read([_|Rest], Column, Value, NewRest) :-
+    select_row_read(Rest, Column, Value, NewRest).
 
 % Update a specific row
 update_csv_row(File, Column, Value, UpdatedRow) :-
@@ -60,20 +61,6 @@ delete_csv_row(File, Column, Value) :-
     csv_read_file(File, Data),
     select_row(Data, Column, Value, DataWithoutRow),
     csv_write_file(File, DataWithoutRow).
-
-% Helper predicate to check if an element exists in a list
-% element_in_list(Element, List) :-
-%     split_string(List, ";", "", ListItems),
-%     member(Element, ListItems).
-
-% Main predicate to select rows
-% select_row_by_list_element([], _, _, []).
-% select_row_by_list_element([Row|Rest], Column, Element, [Row|NewRest]) :-
-%     arg(Column, Row, Value),
-%     element_in_list(Element, Value), !,
-%     select_row_by_list_element(Rest, Column, Element, NewRest).
-% select_row_by_list_element([_|Rest], Column, Element, NewRest) :-
-%     select_row_by_list_element(Rest, Column, Element, NewRest).
 
 read_csv_row_by_list_element(File, Column, Element, Row) :-
     csv_read_file(File, Data),
