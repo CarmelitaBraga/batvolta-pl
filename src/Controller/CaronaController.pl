358850fb4787de81dfde6ec66b57af9177e04aee
CarmelitaBraga <carmelitabraga8@gmail.com>
2024-04-28 23:31:57 -0300

    feat: add desembarcar_passageiro function

--- src/Controller/CaronaController.pl
+++ src/Controller/CaronaController.pl
@@ -1,10 +1,10 @@
 :- module(_,
     [possui_caronas_origem_destino_controller/2,
     mostrar_caronas_disponiveis_origem_destino/3,
+    mostrar_caronas_passageiro/2
     % solicitar_carona_passageiro/5,
     % mostrar_viagem_passageiro/2,
     % cancelar_carona_passageiro/2,
-    mostrar_caronas_passageiro/2
     % desembarcar_passageiro/2,
     % embarcar_passageiro/2,
     % get_viagem_sem_avaliacao/2,
@@ -13,25 +13,32 @@
 
 :- use_module('src/Logic/CaronaLogic.pl').
 
-
 % Carona & passageiro
 
 possui_caronas_origem_destino_controller(Origem, Destino):-
     call(possui_carona_origem_destino(Origem, Destino)).
+% mostrar_caronas_disponiveis_origem_destino("Patos", "Ottawa", Str).
 
 mostrar_caronas_disponiveis_origem_destino(Origem, Destino, Caronas):-
-    mostra_caronas_origem_destino(Origem, Destino, Caronas).
+    mostrar_caronas_origem_destino(Origem, Destino, Caronas).
+% mostrar_caronas_disponiveis_origem_destino("Rio","Sao Paulo", Str).
+
+mostrar_caronas_passageiro(PassageiroCpf, Resultado):-
+    mostrar_caronas_passageiro_participa(PassageiroCpf, Resultado).
+% mostrar_caronas_passageiro("111",R).
+% mostrar_caronas_passageiro_participa("11221122112", Str).
+
+% Recebe PassageiroCpf como String.
+desembarcar_passageiro(IdCarona, PassageiroCpf):- 
+    remover_passageiro(IdCarona, PassageiroCpf).
+% desembarcar_passageiro(0,"11221122112").
+% desembarcar_passageiro(1,"11221122112").
 
 % solicitar_carona_passageiro(CId, PassageiroRef, Origem, Destino, MaybeCaronaEscolhida).
 
 % mostrar_viagem_passageiro(PassageiroCpf, Viagens).
 
 % cancelar_carona_passageiro(CId, PassageiroCpf).
-
-mostrar_caronas_passageiro(PassageiroCpf, Resultado):-
-    mostrar_caronas_passageiro_participa(PassageiroCpf, Resultado).
-
-% desembarcar_passageiro(IdCarona, PassageiroCpf).
 
 % embarcar_passageiro(IdCarona, PassageiroCpf).
 
--- src/Logic/CaronaLogic.pl
+++ src/Logic/CaronaLogic.pl
@@ -1,7 +1,9 @@
 :- module(_,[
     possui_carona_origem_destino/2, 
-    mostra_caronas_origem_destino/3, 
-    mostrar_caronas_passageiro_participa/2
+    mostrar_caronas_origem_destino/3, 
+    mostrar_caronas_passageiro_participa/2,
+    remover_passageiro/2
+    % embarcar_passageiro_carona/2
     ]).
 
 :- use_module('src/Schemas/CsvModule.pl').
@@ -9,8 +11,16 @@
 
 % Définir le chemin du fichier CSV comme une variable globale
 csv_file('database/caronas.csv').
+carona_column(1).
+hora_column(2).
+data_column(3).
 destinos_column(4).
+motorista_column(5).
 passageiros_column(6).
+valor_column(7).
+status_column(8).
+lim_pass_column(9).
+avaliacao_column(10).
 
 % Predicate to check if a route exists from Origem to Destino
 possui_carona_origem_destino(Origem, Destino):-
@@ -27,27 +37,65 @@ ordered_pair_in_list([X,Y], [X|T]) :-
 ordered_pair_in_list(Pair, [_|T]) :-
     ordered_pair_in_list(Pair, T).
 
-mostra_caronas_origem_destino(Origem, Destino, CorrespondingRowsStr):-
+mostrar_caronas_origem_destino(Origem, Destino, CorrespondingRowsStr):-
     csv_file(File),
     destinos_column(Dest_column),
     read_csv_row_by_list_element(File, Dest_column, Origem, Rows),
     findall(Str, (member(Row, Rows), row(_, _, _, Trajeto, _, _, _, _, _, _) = Row, split_string(Trajeto, ";", "", ListaTrajeto), ordered_pair_in_list([Origem, Destino], ListaTrajeto), caronaToStr(Row, Str)), CorrespondingRowsStr).
-
-% Convert all rows in Caronas to strings
-% caronasToStr(Caronas, Strs) :-
-%     findall(Str, (member(Carona, Caronas), caronaToStr(Carona, Str)), Strs).
-
-% mostrar_caronas_passageiro/2,
-% cancelar_carona_passageiro/2,
-% embarcar_passageiro/2,
-% desembarcar_passageiro/2,
-% mostrar_viagem_passageiro/2,
-% get_viagem_sem_avaliacao/2,
-% solicitar_carona_passageiro/5,
-% avaliar_motorista/4
+% mostrar_caronas_passageiro_participa("11221122112", Str).
 
 mostrar_caronas_passageiro_participa(PassageiroCpf, CaronasStr):-
     csv_file(File),
     passageiros_column(Pass_Column),
     read_csv_row_by_list_element(File, Pass_Column, PassageiroCpf, Rows),
     findall(Str, (member(Row, Rows), caronaToStr(Row, Str)), CaronasStr).
+
+% embarcar_passageiro_carona(IdCarona, PassageiroCpf):-
+%     csv_file(File),
+%     carona_column(Carona_Column),
+%     passageiros_column(Pass_Column),
+    % verificar se carona existe
+    % verificar se carona tem espaço
+    % verificar se passageiro existe
+% .
+
+remover_passageiro(IdCarona, PassageiroCpf):-
+    csv_file(File),
+    carona_column(Carona_Column),
+    passageiros_column(Pass_Column),
+    read_csv_row_by_list_element(File, Pass_Column, PassageiroCpf, Caronas),
+    member(row(IdCarona, Hora, Data, Rota, MotoristaCpf, Passageiros, Valor, Status, Vagas, Avaliacao), Caronas),
+    (Caronas == [] ->
+        write('Nenhuma carona correspondente a passageiro encontrada!'),
+        UpdatedRow = row(IdCarona, Hora, Data, Rota, MotoristaCpf, Passageiros, Valor, Status, Vagas, Avaliacao)
+    ;  
+        atom_string(Passageiros, PassageirosString),
+        split_string(PassageirosString, ";", "", PassageirosList),
+        delete(PassageirosList, PassageiroCpf, UpdatedPassageirosList),
+        atomics_to_string(UpdatedPassageirosList, ";", UpdatedPassageiros),
+        UpdatedRow = row(IdCarona, Hora, Data, Rota, MotoristaCpf, UpdatedPassageiros, Valor, Status, Vagas, Avaliacao)    ),
+        update_csv_row(File, Carona_Column, IdCarona, UpdatedRow),
+        write('Passageiro removido com sucesso!').
+% remover_passageiro(2,"11221122112", R).
+
+% solicitar_carona_passageiro/5,
+% solicitaParticiparCarona :: Int -> String -> String -> String -> IO String
+% solicitaParticiparCarona idCarona idPassageiro origem destino = do
+%     -- checar se tem rota
+%     -- checar se tem lugar
+%     -- checar se tem carona
+%     -- checar se o passageiro ja esta
+%     maybeCarona <- getCaronaById [idCarona]
+%     if null maybeCarona
+%         then return "Carona inexistente!"
+%         else do
+%             let carona = head maybeCarona
+%                 rota = getCaminho carona origem destino
+%             if null rota
+%                 then return "Essa carona não possui essa rota!"
+%                 else do
+%                     let mensagem = "O Passageiro: " ++ idPassageiro ++ " solicitou entrar na corrida de id: " ++ show idCarona
+%                     insereNotificacao (motorista carona) idPassageiro idCarona mensagem
+%                     criarViagemPassageiro idCarona False (getCaminho carona origem destino) 0 idPassageiro
+%                     return "Registro de Passageiro em Carona criado com sucesso!"
+
