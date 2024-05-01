6b4eaa74d7f101071587dd55450427df30c3f7cc
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2024-05-01 00:49:25 -0300

    feat: nova regra para converter de lista para String

--- src/Util/Utils.pl
+++ src/Util/Utils.pl
@@ -1,7 +1,8 @@
 :- module(_, [
     parseElement/2,
     insertAtFirst/3,
-    listToRow/2
+    listToRow/2,
+    list_to_string/3
     ]).
 
 parseElement(Element, ParsedElement) :-
@@ -14,3 +15,9 @@ parseElement(Element, ParsedElement) :-
 insertAtFirst(ID, List, [ID|List]).
 
 listToRow(Elements, Row) :- Row =.. [row|Elements].
+
+list_to_string([], Str, Str).
+list_to_string([H|T], Str, R) :-
+    string_concat(Str, H, TempStr),
+    string_concat(TempStr,'\n',NovaStr),
+    list_to_string(T, NovaStr, R).
