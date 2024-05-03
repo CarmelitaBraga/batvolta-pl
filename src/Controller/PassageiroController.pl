5b6dc7203ff11fcfb96b8ed87a019465388d12a7
Cazans <caique.campelo@ccc.ufcg.edu.br>
2024-05-03 01:09:51 -0300

    "refactor: adicionada a função de carregamento de notificações e adequação de funções"

--- src/Controller/PassageiroController.pl
+++ src/Controller/PassageiroController.pl
@@ -2,9 +2,8 @@
     realizar_cadastro_passageiro/8,
     remove_passageiro/3,
     atualizar_cadastro_passageiro/5,
-    visualizar_info_passageiro/1,
-    realizar_login_passageiro/3,
-    salvar_notificacao/5
+    visualizar_info_passageiro/2,
+    realizar_login_passageiro/3
 ]).
 
 :- use_module('../Logic/PassageiroLogic.pl').
@@ -18,14 +17,11 @@ remove_passageiro(CPF, Senha, Passageiro):-
 atualizar_cadastro_passageiro(CPF,SenhaPassada,Coluna,NovoValor,Retorno):-
     atualiza_cadastro_logic(CPF,SenhaPassada,Coluna,NovoValor,Retorno).
 
-visualizar_info_passageiro(CPF):-
-    visualiza_info_logic(CPF).
+visualizar_info_passageiro(CPF, Retorno):-
+visualiza_info_logic(CPF, Retorno).
 
 realizar_login_passageiro(Email, Senha, Passageiro):-
     login_passageiro_logic(Email, Senha, Passageiro).
 
-retornar_notificacao(CPF,Retorno):-
-    recupera_notificacao_logic(CPF,Retorno).
-
-salvar_notificacao(Passageiro, Motorista, Carona, Conteudo, Resposta):-
-    cadastra_notificacao(Passageiro, Motorista, Carona, Conteudo, Resposta).
+%carregar_notificacoes_passageiro():-
+%    carregar_notificacoes_logic().
