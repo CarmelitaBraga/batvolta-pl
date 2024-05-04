20b0d4fd29a4bfcae4f145a45da1a5f85fe19976
Cazans <caique.campelo@ccc.ufcg.edu.br>
2024-05-04 20:20:47 -0300

    "feat: adicionado o top 5 no controller"

--- src/Controller/DashboardController.pl
+++ src/Controller/DashboardController.pl
@@ -7,3 +7,6 @@ buscar_melhores_motoristas(MelhoresMotoristas):-
 
 buscar_melhores_motoristas_regiao(Regiao, MelhoresMotoristas):-
     get_top_motoristas_by_regiao(Regiao, MelhoresMotoristas).
+
+buscar_destinos_mais_frequentes(Destinos):-
+    top_5_destinos(Destinos).
